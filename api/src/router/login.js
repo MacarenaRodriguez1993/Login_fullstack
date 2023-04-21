259dd51d90f4106a9adc8fe3f9954df0e10fed1b
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2023-04-21 18:25:26 -0300

    se agrego encriptacion y jwt para generar token

--- api/src/router/login.js
+++ api/src/router/login.js
@@ -1,23 +1,59 @@
 const Router = require("express");
 const router = Router();
 const User = require("../model/Login");
-
-router.get("/", (req, res) => {});
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ err: error.message });
+  }
+});
 
 router.get("/:id", (req, res) => {});
 
 router.post("/login", async (req, res) => {
-  console.log("ola");
-  const data = req.body;
+  const { email, password } = req.body;
   try {
-    await User.create(data);
-    res.status(201).json(data);
+    const user = await User.findOne({ where: { email } });
+    if (user === null) {
+      throw new Error("El usuario no existe");
+    }
+    const correctPassword = await bcrypt.compare(password, user.password);
+    if (correctPassword) {
+      const infoForToken = {
+        id: user.id,
+        email: user.email,
+        firstname: user.firstname,
+        lastname: user.lastname,
+      };
+      const token = jwt.sign(infoForToken, "magicSecret");
+      res.status(200).json(token);
+    } else throw new Error("credenciales incorrectas");
   } catch (error) {
     res.status(404).json({ err: error.message });
   }
 });
 
-router.post("/register", (req, res) => {});
+router.post("/register", async (req, res) => {
+  const { firstname, lastname, email, birthDate } = req.body;
+  const passwordHash = bcrypt.hashSync(req.body.password, 10);
+  try {
+    await User.create({
+      firstname,
+      lastname,
+      email,
+      birthDate,
+      password: passwordHash,
+    }).then((u) => {
+      res.status(201).json(u);
+    });
+  } catch (error) {
+    res.status(404).json({ err: error.message });
+  }
+});
 router.put("/user", (req, res) => {});
 router.delete("/user/:id", (req, res) => {});
 
